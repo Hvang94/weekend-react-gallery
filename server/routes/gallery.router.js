c1aeb1ddfcadc9ffc623404f9d6a758a1c8d59e9
Hmoovzong Vang <hmoovzongvang@gmail.com>
2024-01-15 22:25:53 -0600

    Set up GET routes and bare bones PUT routes.

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,14 +1,40 @@
 const express = require('express');
+const { s } = require('vitest/dist/reporters-O4LBziQ_');
 const router = express.Router();
 
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
   // code here
+ 
+  // ! Need queryParams and queryText
+
+    // send UPDATE to DB
+    pool
+      .query(queryText, queryParams)
+      // then send ok status
+      .then((result) => {
+        res.sendStatus(200);
+      })
+      .catch((error) => {
+        console.log("error:", error);
+      });
 });
 
 // GET /gallery
 router.get('/', (req, res) => {
   // code here
+  const sqlText = `SELECT * FROM "galary"`;
+
+  pool.query(sqlText)
+  .then((result) => {
+    console.log("server GET", result);
+res.send(result.rows)
+  })
+  .catch((error) => {
+console.log("error in GET");
+console.log(error);
+res.sendStatus(500)
+  })
 });
 
 module.exports = router;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,49 @@
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Header from "../Header/Header";
 
 function App() {
-    return (
-      <div>
-        <header>
-          <h1>React Gallery</h1>
-        </header>
+  const [galleryItems, setGallaryItems] = useState([]);
 
-        <p>The gallery goes here!</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
-    );
+  const fetchImage = () => {
+    axios
+      .get("/api/gallery")
+      .then((response) => {
+        console.log("GET response good");
+        console.log(respons.data);
+        setGallaryItems(response.data)
+      })
+      .catch((error) => {
+        console.log("GET failed");
+        console.log(error);
+      });
+  };
+
+  const likeImage = (imageId) => {
+    axios
+      .put(`/api/gallary/${imageId}`)
+      .then((response) => {
+        fetchImage();
+      })
+      .catch((error) => {
+        console.log("Error in PUT", error);
+      });
+  };
+
+  useEffect(() => {
+    fetchImage();
+  }, []);
+
+  return (
+    <div>
+      <Header />
+
+      <p>The gallery goes here!</p>
+      <img src="images/goat_small.jpg" />
+      <button>Like</button>
+    </div>
+  );
 }
 
 export default App;
